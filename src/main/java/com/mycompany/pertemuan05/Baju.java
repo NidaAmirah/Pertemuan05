aa80fa254d13f86b0bad9c119f181aebb5e168d8
NidaAmirah <amirahnida03@gmail.com>
2023-05-10 19:48:32 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/pertemuan05/Baju.java
@@ -0,0 +1,19 @@
+package com.mycompany.pertemuan05;
+
+
+public class Baju {
+    String merk;
+    String ukuran;
+    String warna;
+    
+    
+        
+    
+void info(){    
+    System.out.println("Nama merk : " +merk);
+    System.out.println("ukuran : "+ukuran);
+    System.out.println("warna baju : "+warna);
+    System.out.println("");
+    
+}
+}
---
+++ src/main/java/com/mycompany/pertemuan05/main.java
@@ -0,0 +1,29 @@
+package com.mycompany.pertemuan05;
+
+
+public class main {
+    public static void main(String[] args) {
+        
+        Baju Nida= new Baju();
+        Nida.merk= "Al-Zahra";
+        Nida.ukuran= "M";
+        Nida.warna= "Merah Maron";
+        
+        
+        Baju Nidaa= new Baju();
+        Nidaa.merk= "Al-Zahra";
+        Nidaa.ukuran= "L";
+        Nidaa.warna= "Hitam";
+        
+        
+        Baju Nidaaa= new Baju();
+        Nidaaa.merk= "Al-Zahra";
+        Nidaaa.ukuran= "XL";
+        Nidaaa.warna= "Pink Dusty";
+        
+        
+        Nida.info();
+        Nidaa.info();
+        Nidaaa.info();
+    }
+}
